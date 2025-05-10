89361a51b20bc1cfc51e3f88aacaa032a7f4785f
johnlestercurro <johnlestercurro@gmail.com>
2025-05-10 09:36:33 +0800

    not found page

---
+++ src/components/404/NotFound.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="not-found">
+    <h1>404 - Page Not Found</h1>
+    <p>The page you're looking for doesn't exist.</p>
+    <router-link to="/">Return to Home</router-link>
+  </div>
+</template>
+
+<style scoped>
+.not-found {
+  text-align: center;
+  padding: 2rem;
+  margin-top: 5rem;
+}
+</style>
